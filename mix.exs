396ffd3eb323c1dae2da06124301e86cc08a1eb0
Edgar Pino <edgarpino@me.com>
2017-11-02 22:30:21 -0600

    Update mix packages

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule SEDailyRT.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:html_sanitize_ex, "~> 1.3"}
     ]
   end
 
